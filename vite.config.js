f0dd5769b4b7dc3e956c89e224b9240f76b44c3c
Aaliya Mubashira <aaliyamubashira1124@gmail.com>
2025-10-06 01:12:43 +0530

    fix: Correct deployment base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-   base: '/varsha-raksha/', 
+   base: '/Varsha-Raksha/', 
   plugins: [react()],
 })
